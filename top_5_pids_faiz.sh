e042bf03fc334103d987988f28a0dcb4f3aa73b7
user1 <user1@email.com>
2025-03-08 12:13:13 +0000

    adding proper comments in the script

--- top_5_pids_faiz.sh
+++ top_5_pids_faiz.sh
@@ -1,5 +1,6 @@
 #!/bin/env bash
 
-#display the os version on the vm
+#display the top processes according to the cpu usage. limit the output to 6 lines.
+#first line for header and 5 lines for top processes. 
 
 ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -6
